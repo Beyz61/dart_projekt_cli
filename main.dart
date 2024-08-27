e19a8c87a059074079a728b8781b23436b853b4c
beyzanur <beyzanur.o@icloud.com>
2024-08-27 14:37:11 +0200

    Anmeldung

--- main.dart
+++ main.dart
@@ -1,3 +1,14 @@
+import 'dart:io';
+
 void main() {
-  print("Foodie");
+  print("Bitte gib deinen Benutzernamen ein");
+  String? benutzername = stdin.readLineSync();
+  print("Bitte gib deinen Passwort ein:");
+  String? passwort = stdin.readLineSync();
+
+  if (benutzername == "beyz" && passwort == "6161") {
+    print("Benutzername erfolgreich eingegeben");
+  } else {
+    print("Falscher Benutzername oder Passwort");
+  }
 }
